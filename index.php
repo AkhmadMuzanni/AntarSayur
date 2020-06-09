f4bcbec9511917239908804e06bb4c9e22ed81c8
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2020-06-09 22:21:16 +0700

    Header and Nav

--- index.php
+++ index.php
@@ -1,8 +1,58 @@
 <html>
     <head>
-        <title>Antarsayur</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>AntarSayur</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/bootstrap.css" rel="stylesheet" />
+        <link href="css/bootstrap-grid.css" rel="stylesheet" />
+        <link href="css/bootstrap-reboot.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet" />
     </head>
     <body>
-        Testing
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+                <a class="navbar-brand" href="#">AntarSayur.id</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PRODUCT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">KONTAK</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">LOGIN</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    
+        <header class="header">
+            <div class="container">
+                <h3 class="text-center text-white" id="title">Selamat Datang</h3>
+                <h1 class="text-center text-white" id="subtitle">AntarSayur.id</h1>
+            </div>
+        </header>
+        <section class="page-section" id="product">
+            <div class="container">
+                <h1 class="text-center">Product</h1>
+            </div>
+        </section>
+        <footer>
+
+        </footer>
     </body>
 </html>
